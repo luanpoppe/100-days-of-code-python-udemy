baa7ad4725c6a27b56c41e98ebf39c790abb5b07
luanpoppe <luanpoppe@gmail.com>
2025-01-14 15:41:16 -0300

    refactor: pequena melhoria no projeto 01

--- 01/task.py
+++ 01/task.py
@@ -1,7 +1,5 @@
 greeting = "Welcome to the Band Name Generator"
-city = input(greeting + "\nWhat's the name of the city you grow up in? ")
-print(city)
-pet_name = input("What's your pet's name? ")
-print(pet_name)
+city = input(greeting + "\nWhat's the name of the city you grow up in?\n")
+pet_name = input("What's your pet's name?\n")
 
 print("Your band name could be " + city + " " + pet_name)
