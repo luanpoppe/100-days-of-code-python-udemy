30783617a7b10abf231bb0bc4cf61df3a37b2cd1
luanpoppe <luanpoppe@gmail.com>
2025-01-14 15:39:44 -0300

    initial commit

---
+++ 01/task.py
@@ -0,0 +1,7 @@
+greeting = "Welcome to the Band Name Generator"
+city = input(greeting + "\nWhat's the name of the city you grew up in? ")
+print(city)
+pet_name = input("What's your pet's name? ")
+print(pet_name)
+
+print("Your band name could be " + city + " " + pet_name)
